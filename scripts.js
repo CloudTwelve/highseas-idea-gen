1b961e18d07aee8b898f454177d4a491dfaf271e
CloudTwelve <94795982+CloudTwelve@users.noreply.github.com>
2025-02-02 18:04:11 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,27 @@
-let webIdeas = [];
-let themes = ["retro", "galaxy", "pastel", "cottagecore", "y2k", "black-and-white", "modern"];
-let randomGen = (num) => {
-    
-}
+let genButton = document.querySelector("button");
+let dropdown = document.querySelector("#levels");
+let themeCont = document.querySelector("#themecont");
+let ideaCont = document.querySelector("#ideacont");
+let currentArr;
+
+genButton.addEventListener('click', () => {
+  switch (dropdown.value) {
+    case "Beginner":
+      currentArr = beginnerWebsites;
+      break;
+    case "Intermediate":
+      currentArr = intermediateWebsites;
+      break;
+    case "Advanced":
+      currentArr = advancedWebsites;
+      break;
+    default:
+      currentArr = beginnerWebsites;
+} 
+  let [func, imp] = genRandomXY(currentArr);
+  let [theme, effect] = genRandomXY(themeEffects);
+  let ideaStr = "Hmm... What about a website that " + func + " and " + imp + "?";
+  let themeStr = "A(n) " + theme + " theme and a(n) " + effect + ".";
+  ideaCont.textContent = ideaStr;
+  themeCont.textContent = themeStr;
+});
